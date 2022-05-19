3133eba6007d87adbb696803cc032a3a7a7ea56c
DLineAm <pochta23114@gmail.com>
2022-05-19 14:02:34 +0300

    Добавьте файлы проекта.

---
+++ WpfApp2/App.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static PseudoDataBase db = new PseudoDataBase();
+    }
+    public class PseudoDataBase
+    {
+        public List<User> Users { get; set; } = new List<User>
+        {
+            new User
+            {
+                Name = "Abooba",
+                Login = "123",
+                Password = "123"
+            },
+            new User
+            {
+                Name = "Baobab",
+                Login = "1",
+                Password = "1"
+            },
+            new User
+            {
+                Name = "Bulba",
+                Login = "2",
+                Password = "2"
+            },
+        };
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer _lockTimer = new DispatcherTimer
+        {
+            Interval = TimeSpan.FromSeconds(15)
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            CaptchaPanel.Visibility = Visibility.Collapsed;
+            _lockTimer.Tick += delegate { this.IsEnabled = true; _lockTimer.Stop();  };
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            var login = LoginBox.Text;
+            var password = PasswordBox.Text;
+            var captchaVisibility = CaptchaPanel.Visibility;
+            var enteredCaptcha = CaptchaTB.Text;
+            var generatedCaptcha = CaptchaTBl.Text;
+
+            if (string.IsNullOrWhiteSpace(login) ||
+                string.IsNullOrWhiteSpace(password) ||
+                captchaVisibility == Visibility.Visible && string.IsNullOrWhiteSpace(enteredCaptcha) )
+            {
+                MessageBox.Show("Все поля должны быть заполнены!", "Ошибка");
+                return;
+            }
+
+            if (captchaVisibility == Visibility.Visible &&
+                enteredCaptcha != generatedCaptcha)
+            {
+                MessageBox.Show("Введенная каптча не совпадает со сгенерированной, повторите попытку через 15 сек", "Ошибка");
+
+                this.IsEnabled = false;
+                _lockTimer.Start();
+
+                CaptchaTBl.Text = GenerateCaptcha();
+
+                return;
+            }
+
+            var user = App.db.Users.FirstOrDefault(u => u.Login == login && u.Password == password);
+
+            if (user == null)
+            {
+                MessageBox.Show("Неверное имя пользователя и/или пароль", "Ошибка");
+
+                CaptchaPanel.Visibility = Visibility.Visible;
+                CaptchaTBl.Text = GenerateCaptcha();
+
+                return;
+            }
+
+            var window = new Window1(user);
+            window.Show();
+            this.Close();
+        }
+
+        private string GenerateCaptcha()
+        {
+            const string symbols = "QWERTYUIOPASDFGHJKLZXCVBNM1234567890";
+            var result = "";
+            var random = new Random();
+            for (var i = 0; i < 4; i++)
+            {
+                result += symbols[random.Next(symbols.Length)];
+            }
+
+            return result;
+        }
+
+        private void CaptchaRefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            CaptchaTBl.Text = GenerateCaptcha();
+        }
+    }
+}
